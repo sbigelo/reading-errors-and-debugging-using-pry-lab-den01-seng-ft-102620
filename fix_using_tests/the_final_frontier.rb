0fcd250b1137f3283447185e3133c87fce7cb121
Sean Bigelow <sean.bigelow1@gmail.com>
2020-08-28 19:26:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,10 +7,10 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def engage(date)
-  binding.pry
   puts state_log(date)
   date = generate_star_date
 end
